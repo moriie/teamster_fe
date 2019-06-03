8415c5e28203831f45012869ee1929eed681d2a4
Jeff Yu <jwy5140@gmail.com>
2019-06-03 17:24:20 -0400

    Added basic navbar/sidebar containers + CSS for frameworking purposes.

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -1,10 +1,22 @@
-import { React, useState} from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
-    
+
     const [display, setDisplay] = useState(true);
 
-
+    return (
+    <div id='navbar'>
+        <h1>Navbar</h1>
+        <span> 
+            <img src='' name='teamster-logo' alt='' />
+            <button>Button 1</button> 
+            <button>Button 2</button> 
+            <button>Button 3</button> 
+            <button>Button 4</button> 
+            <button>Button 5</button>
+            <img src='https://pm1.narvii.com/7127/78ea6e082f2e9848ce81c0edbab1e6c4b279d85ar1-712-608v2_128.jpg' alt='' className='profile-btn'/> 
+        </span>
+    </div>)
 }
 
 export default Navbar;
---
+++ src/containers/sidebar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Sidebar = () => {
+
+    const [display, setDisplay] = useState(true)
+
+    return (
+        <div id='sidebar'>
+            <h1>Sidebar</h1>
+            <ul className='goals-list'>Goals</ul>
+        </div>
+    )
+
+}
+
+export default Sidebar
