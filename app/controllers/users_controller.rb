92529adf9d5d4874ac2d04b8fa37b0a50f19d940
jwy5140 <jwy5140@gmail.com>
2019-06-07 02:53:08 -0400

    added routes, worked toward completion of controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def create
+        @user = User.create(u_params)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def u_params(*args)
+        params.require(:user).permit(args)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:create, update, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
