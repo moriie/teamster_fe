6f6a98bffa6f952c0de36526c2b9677897004ec6
jwy5140 <jwy5140@gmail.com>
2019-08-26 02:26:45 -0400

    Update backend validation messages.

--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
     has_many :network_goals, through: :network, source: :partner, source_type: 'Goal'
 
     validates :username, uniqueness: { case_sensitive: true }, length: { minimum: 4, maximum: 14 }
-    validates :age, exclusion: { in: 0..18 }
-    validates :email, format: { with: /\S+@[a-z]+.com/i }
+    validates :age, exclusion: { in: 0..18, message: 'must be 18 or older to use this application.' }
+    validates :email, format: { with: /\S+@[a-z]+.com/i }, uniqueness: { case_sensitive: false }
     validates :bio, length: { maximum: 180 }
-    validates :location, format: { with: /[0-9]{5}/ }
+    validates :location, format: { with: /[0-9]{5}/, message: 'must be standard 5 digit US zip code' }
     
 end
