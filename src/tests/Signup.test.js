e301bf4242924bc1913eb2f3fa24de5694a63ada
jwy5140 <jwy5140@gmail.com>
2019-10-01 23:48:53 -0400

    Added test for Signup page error modal. Currently broken due to error concerning NavLink.

--- src/tests/Signup.test.js
+++ src/tests/Signup.test.js
@@ -1,4 +1,7 @@
-import { unmountComponentAtNode } from "react-dom";
+import React from 'react'
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils"
+import Signup from '../components/nonAuth/signup'
 
 let container = null;
 beforeEach(() => {
@@ -6,6 +9,42 @@ beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
 });
+
+// it("validates initial desired user credentials", async ()=>{
+//   const badUsername = {
+//     username: "123",
+//     password: "1",
+//     email: "email@email.com",
+//     age: "21",
+//     location: "11111"
+//   }
+
+//   // const badPassword = {
+
+//   // }
+
+//   jest.spyOn(global, "fetch").mockImplementation(()=>
+//     Promise.resolve({
+//       json: () => Promise.resolve(badUsername)
+//     })
+//   )
+// })
+
+it("renders error messages from the backend in a modal", async ()=>{
+  const response = {"error": "This is an error message."}
+
+  jest.spyOn(global, "fetch").mockImplementation(()=>
+    Promise.resolve({
+      json: () => Promise.resolve(response)
+    })
+  )
+
+  await act(async () => {
+    render(<Signup />, container)
+  })
+
+  expect(container.querySelector(".error").textContent).toBe(response.error)
+})
 
 afterEach(() => {
   // cleanup on exiting
